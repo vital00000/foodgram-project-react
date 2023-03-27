bc7a7e0a8bf19037a10412219ff33901422c665b
Виталий Станкевич <vitaliy.v.stankevich>
2023-03-27 23:53:11 +0300

    sdfsd

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
@@ -44,8 +41,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-    @receiver(post_save, sender=AbstractUser)
-    def create_auth_token(sender, instance=None, created=False, **kwargs):
-        if created and instance.is_superuser:
-            Token.objects.create(user=instance)
