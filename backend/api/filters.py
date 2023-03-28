4d956172ab75c2a8ce0450b352d5af6fe73a17fe
Виталий Станкевич <vitaliy.v.stankevich>
2023-03-28 21:26:12 +0300

    dfg

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,9 +12,6 @@ class RecipeFilter(django_filters.FilterSet):
         queryset=Tag.objects.all(),
     )
     author = django_filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filter.BooleanFilter(method='get_favorite')
-    is_in_shopping_cart = filter.BooleanFilter(
-        method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
