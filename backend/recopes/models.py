f538d8cc299f7267a049c2f3b107b3bfb0a0d8f2
Виталий Станкевич <vitaliy.v.stankevich>
2023-03-30 11:18:21 +0300

    Проект

--- backend/recopes/models.py
+++ backend/recopes/models.py
@@ -83,11 +83,12 @@ class Recipe(models.Model):
         verbose_name='Дата публикации',
         auto_now_add=True,
     )
+    created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
-        ordering = ('pub_date',)
+        ordering = ('created_date',)
 
     def __str__(self):
         return self.name
