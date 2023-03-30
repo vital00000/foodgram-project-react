c1f2c8872af5a90b9d1701a4a6fbb675fdba71fc
Виталий Станкевич <vitaliy.v.stankevich>
2023-03-30 20:55:17 +0300

    Project

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -34,7 +34,7 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
-        if request.user.is_authenticated and request.user.is_anonymous:
+        if request.user.is_anonymous:
             return False
         return Subscription.objects.filter(
             user=request.user, author=obj
